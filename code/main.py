b624d9ae42d9e45fce4d7a56df5c8698205fdb17
wxx <614610440@qq.com>
2019-02-14 23:29:36 +0800

    解决'嗯'无法合成的问题

--- code/main.py
+++ code/main.py
@@ -7,7 +7,7 @@ def main(argv):
     # if len(argv) < 2:
     #     print ("请输入语句")
     #     return 
-    synthesis("你好司法四方阿哥世界,阿斯大赛，萨嘎三个毫无疑问")
+    synthesis("嗯你好司法四方阿哥世界,阿斯大赛，萨嘎三个毫无疑问")
 
 if __name__ == '__main__':
     main(sys.argv)
